5dc7eb5d67e4165f428c4c065b87aeca920ce3e7
hyst329 <trolleybus.1329@gmail.com>
2014-11-01 16:48:08 +0300

    Version fix

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-from f4ver import version
-
-setup(
-    name="PyF4",
-    version="%d.%d.%d" % (version["major"], version["minor"], version["patch"]),
-    ext_modules=cythonize("f4*.py"), requires=['Cython']
-)
+# from distutils.core import setup
+# from Cython.Build import cythonize
+# from f4ver import version
+#
+# setup(
+#     name="PyF4",
+#     version="%d.%d.%d" % (version["major"], version["minor"], version["patch"]),
+#     ext_modules=cythonize("f4*.py"), requires=['Cython']
+# )
