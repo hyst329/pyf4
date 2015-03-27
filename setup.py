20fa00a7d0e387bdfee271515bb811567d7c2efa
hyst329 <trolleybus.1329@gmail.com>
2015-03-27 14:45:35 +0300

    fixed name in setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ import sys
 from cx_Freeze import setup, Executable
 from f4ver import verstrs
 
-setup(name="pyf4",
+setup(name="pyf4helen",
       version=verstrs,
-      description="Python implementation of the F4 programming language",
+      description="Python implementation of the F4/Helen programming language",
       options={
           "build_exe": {
               "init_script": "Console",
